1dbdcba2c9da9ec9c320b0ab05fca24b22024094
tanjil <tanjil14@cse.pstu.ac.bd>
2021-09-02 09:39:29 +0600

    solved mixed content error

--- main.js
+++ main.js
@@ -1,53 +1,85 @@
-// select require all html elements
+//control spinner visibility
+const toggleSpinner = (displayStyle) => {
+  document.getElementById("spinner").style.display = displayStyle;
+};
+//control error throwing
+const toggleSearchResult = (displayStyle) => {
+  document.getElementById("search-results").style.display = displayStyle;
+};
 
+// search function, given input by user
 const getBooks = () => {
   const inputText = document.getElementById("input-box");
   const searchText = inputText.value;
-  const URL = `http://openlibrary.org/search.json?q=${searchText}`;
-  loadData(URL);
+  const errorBox = document.getElementById("error-box");
+  //   check is input empty
+  if (searchText === "") {
+    errorBox.innerHTML = `<h5 class='text-center'>You didn't enter any book</h5>`;
+  } else {
+    //   clear error msg
+    errorBox.textContent = "";
+    //   display spinner
+    toggleSpinner("block");
+    toggleSearchResult("none");
+    loadData(searchText);
+    //clear input box
+    inputText.value = "";
+  }
 };
 
-const loadData = async (url) => {
-  const res = await fetch(url);
-  const data = await res.json();
-  displayTotalNum(data);
-  displayResults(data.docs);
+//load data by api
+const loadData = async (searchText) => {
+  const URL = `https://openlibrary.org/search.json?q=${searchText}`;
+  try {
+    const res = await fetch(URL);
+    const data = await res.json();
+    displayTotalNum(data);
+    displayResults(data.docs);
+  } catch (e) {
+    toggleSpinner("none");
+    document.getElementById("error-box").innerHTML = `<h5 class='text-center'>${e.message},
+    please try again!</h5>`;
+  }
 };
+// display total number of found
 const displayTotalNum = (totalResults) => {
   document.getElementById("total-results").innerHTML = `
 <h6>About ${totalResults.numFound} Results</h6>`;
 };
+
+//  display books info
 const displayResults = (results) => {
-  //   results.forEach(result=>{
-  //       const authorNames=result.author_name;
-  //       authorNames?.forEach(author=>{
-  //         console.log(author)
-  //       })
-
-  //   })
-
-  const allResults = document.getElementById("all-results");
-  results.forEach((result) => {
-    const authors = result.author_name;
-  
-      const div=document.createElement('div');
-      div.classList.add('col');
-      div.innerHTML=` <div class="card h-100">
-      <img src="..." class="card-img-top" alt="..." />
+  const allResultsDiv = document.getElementById("all-results");
+  //clear previous results
+  allResultsDiv.textContent = "";
+  // traverse all data in array
+  results.forEach((result, index) => {
+    //   handle load all data on ui,just load 30
+    if (index < 30) {
+      const div = document.createElement("div");
+      div.classList.add("col");
+      div.innerHTML = ` <div class="card h-100">
+      <img src="https://covers.openlibrary.org/b/id/${result.cover_i}-M.jpg" class="card-img-top" alt="Image" />
       <div class="card-body">
         <dl class="row">
           <dt class="col-sm-4">BookName:</dt>
           <dd class="col-sm-8">${result.title}</dd>
 
           <dt class="col-sm-4">Author:</dt>
-          <dd class="col-sm-8">${authors?.forEach((author) => author)}</dd>
+          <dd class="col-sm-8">${result.author_name ? result.author_name : "Author not found"}</dd>
 
           <dt class="col-sm-4">Publication Date:</dt>
-          <dd class="col-sm-8">${result.publish_year}</dd>
+          <dd class="col-sm-8">${result.publish_date ? result.publish_date : "Published date not found"}</dd>
+
+          <dt class="col-sm-6">First Publish Year:</dt>
+          <dd class="col-sm-6">${result.first_publish_year ? result.first_publish_year : " Date not found"}</dd>
         </dl>
       </div>
     </div>`;
-     allResults.appendChild(div);
-
-  })
+      allResultsDiv.appendChild(div);
+    }
+  });
+  //hide spinner
+  toggleSpinner("none");
+  toggleSearchResult("block");
 };
